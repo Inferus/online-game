6acf3bbe281a23401b69bf346d11ecf346518cdc
Moonlization <seva2409@gmail.com>
2022-02-18 23:40:15 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+const [bet, setbet] = useState(0)
+const [amount, setamount] = useState(10)
+const [message, setmessage] = useState('Place a bet!')
+const [loading, setloading] = useState(false)
+  const game = () =>{
+    if (bet === 0) return setmessage('Bet can not be 0')
+    if (bet>amount) return setmessage('You have low funds')
+const decide = Math.random()>0.5
+setloading(true)
+setamount(prev => prev - bet)
+setmessage('Wait a bit... Wish you luck...')
+setTimeout(()=>{if (decide) {
+  setloading(false)
+  setmessage('You won!')
+return setamount(prev => prev+bet*1.5)}
+ setmessage('You lose, try again')
+ setloading(false)
+  }, 1000)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Your bet is {bet}</h1>
+
+    <button onClick={()=>setbet(prev=>prev+1)}>Increase bet</button>
+    <button  onClick={()=>setbet(prev=> prev === 0 ? 0 :prev-1)}>Decrease bet</button>
+    <button disabled={loading} onClick={game}>Bet</button>
+    <h1>Your gold: {amount}</h1>
+    {message}
     </div>
   );
 }
